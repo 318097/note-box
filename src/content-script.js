3d2e4d73b360ceba4bdd8674519471aa1a1b514a
Mehul Lakhanpal <318097@gmail.com>
2020-02-14 15:05:22 +0530

    Update: Common click listener, find nodes only for github md files

---
+++ src/content-script.js
@@ -0,0 +1,54 @@
+window.onload = () => {
+  const localDB = localStorage.getItem("note-box");
+  const database = JSON.parse(localDB || "{}");
+  // document.body.style.backgroundColor = "#f3f3f3";
+  console.log("Hi, from chrome link..");
+
+  const mdFiles = document.querySelectorAll(".Box .Box-body");
+  console.log(`markdown body - ${mdFiles.length} matches :`, mdFiles);
+  const initialize = () => {
+    // const listCollection = mdFiles[0].querySelectorAll("ol");
+
+    // listCollection.forEach(list => {
+    // const links = list.querySelectorAll("li a");
+    const links = mdFiles[0].querySelectorAll("li a");
+    links.forEach(link => {
+      const spanElement = document.createElement("span");
+      spanElement.setAttribute("class", "checkboxContainer");
+
+      const linkUrl = link.href;
+      const isChecked = database[linkUrl];
+
+      const checkbox = document.createElement("input");
+      checkbox.checked = isChecked;
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.setAttribute("class", "checkbox");
+      checkbox.setAttribute("data-url", linkUrl);
+
+      spanElement.appendChild(checkbox);
+
+      const listElement =
+        link.parentNode.nodeName === "LI"
+          ? link.parentNode
+          : link.parentNode.parentNode;
+      listElement.append(spanElement);
+    });
+    // });
+  };
+
+  const handleClick = event => {
+    if (
+      event.target.type === "checkbox" &&
+      event.target.className === "checkbox"
+    ) {
+      const key = event.target.getAttribute("data-url");
+      const checkedValue = event.target.checked;
+      database[key] = checkedValue;
+      localStorage.setItem("note-box", JSON.stringify(database));
+      console.log("foundd....");
+    }
+  };
+  document.addEventListener("click", handleClick);
+
+  initialize();
+};
